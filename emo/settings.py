b24f11f68ca7eced04880aaff2d2300660067263
zhangsuosheng <zhangsuosheng9@163.com>
2018-04-02 19:37:02 +0800

    change the database settings

--- emo/settings.py
+++ emo/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'HOST':'127.0.0.1',
         'PORT':'3306',
         'USER':'root',
-        'PASSWORD':'zhangsuosheng',
+        'PASSWORD':'',
         'OPTIONS':{
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
         },
